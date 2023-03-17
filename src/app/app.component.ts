9a59f24dd0596a87d6cc85a818b72a1a8b8c6545
krushna <krushna.sontakke6345@gmail.com>
2023-03-17 20:08:56 +0530

    FinalCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'system_management';
+
+  
+
+
+  showConfiguration() {
+  document.getElementById("Configure-dia");
+  document.querySelector(".overlay");
+}
+
+  saveConfig()
+  {
+    const connfigData=document.getElementsByClassName(".overlay");
+  }
+
+  showAlart()
+  {
+    document.getElementsByClassName(".webServerAlart");
+    document.getElementsByClassName(".loadServer");
+    document.querySelector(".overlay");
+  }
 }
